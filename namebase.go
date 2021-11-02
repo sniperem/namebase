19754074020e5f203c37fc027eb3eafa2fa90318
Di Wu <eaglewudi@gmail.com>
2021-11-02 13:58:40 +0800

    Check status ok code first

--- namebase.go
+++ namebase.go
@@ -540,6 +540,12 @@ func (nb *Namebase) do(method, endpoint string, params map[string]interface{}, s
 	// dump, _ = httputil.DumpResponse(resp, true)
 	// log.Print("raw resp: ", string(dump))
 
+	if resp.StatusCode != 200 {
+		dump, _ := httputil.DumpResponse(resp, true)
+		return nil, fmt.Errorf("http code: %d, body: %s",
+			resp.StatusCode, string(dump))
+	}
+	
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
@@ -552,12 +558,6 @@ func (nb *Namebase) do(method, endpoint string, params map[string]interface{}, s
 
 	if err := json.Unmarshal(body, &result); err == nil && result.Code != "" {
 		return nil, errors.New(result.Message)
-	}
-
-	if resp.StatusCode != 200 {
-		dump, _ := httputil.DumpResponse(resp, true)
-		return nil, fmt.Errorf("http code: %d, body: %s",
-			resp.StatusCode, string(dump))
 	}
 
 	return body, err
